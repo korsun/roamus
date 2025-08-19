ba79eb0d39b1d4a18248a28d5af261834054e088
Kirill Korsun <korsun.me@gmail.com>
2025-08-19 15:23:49 +0200

    Delete apps/client/src/domains/Map directory

--- apps/client/src/domains/Map/Map.tsx
+++
@@ -1,9 +0,0 @@
-import { useMap } from '@/shared/services';
-
-import s from './Map.module.css';
-
-export const Map = () => {
-  const { mapRef } = useMap({ styles: s });
-
-  return <div ref={mapRef} className={s.mapContainer} />;
-};
